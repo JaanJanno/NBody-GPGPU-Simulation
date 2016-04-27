c6a6bc31be47b00f41f45439704a022de62b8c5f
Jaan Janno <jaan911@ut.ee>
2016-04-27 21:47:04 +0300

    Unsuccesful optimization attempt. But the new alternative supports more bodies.

---
+++ src/ee/ut/jjanno/gpusimulation/GravitySystemAdvancedUnstackDuoKernel.java
@@ -0,0 +1,149 @@
+package ee.ut.jjanno.gpusimulation;
+
+import com.amd.aparapi.Kernel;
+
+import ee.ut.jjanno.simulation.Gravity;
+
+class GravitySystemAdvancedUnstackDuoKernel extends Kernel {
+
+	private static final float CUTOFF = 0.5f;
+
+	private float[] points;
+	private float[] tree;
+	private int[] refs;
+	private int realSize;
+
+	@Override
+	public void run() {
+		int pointIndex = getGlobalId() * 7;
+
+		if (getGlobalId() < realSize) {
+
+			points[pointIndex + 4] = 0;
+			points[pointIndex + 5] = 0;
+
+			int vertex = 1;
+			int traversed = 1;
+
+			while (vertex != 0) {
+				int vertexPointer = (vertex - 1) * 4;
+				int refPointer = (vertex - 1) * 5;
+				boolean open = true;
+
+				if (vertex != traversed) {
+					open = false;
+					int pointer2 = refs[refPointer + 2];
+					if (pointer2 > traversed && pointer2 != 0) {
+						vertex = pointer2;
+						traversed = vertex;
+					} else {
+						int pointer3 = refs[refPointer + 3];
+						if (pointer3 > traversed && pointer3 != 0) {
+							vertex = pointer3;
+							traversed = vertex;
+						} else {
+							int pointer4 = refs[refPointer + 4];
+							if (pointer4 > traversed && pointer4 != 0) {
+								vertex = pointer4;
+								traversed = vertex;
+							} else {
+								vertex = refs[refPointer];
+							}
+						}
+					}
+				} 
+				if (open && vertex == traversed) {
+					float x1 = points[pointIndex];
+					float y1 = points[pointIndex + 1];
+					float x2 = tree[vertexPointer];
+					float y2 = tree[vertexPointer + 1];				
+
+					float d = tree[vertexPointer + 3];
+
+					float sqDistance = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+
+					if (sqDistance != 0 && (d * d) / sqDistance <= CUTOFF) {
+						float m1 = points[pointIndex + 6];
+						float m2 = tree[vertexPointer + 2];
+						computeAccelerationVector(m1, m2, x1, y1, x2, y2, pointIndex, sqrt(sqDistance));
+						vertex = refs[refPointer];
+					} else {
+						int pointer1 = refs[refPointer + 1];
+						if (pointer1 > traversed) {
+							vertex = pointer1;
+							traversed = vertex;
+						} else {
+							int pointer2 = refs[refPointer + 2];
+							if (pointer2 > traversed) {
+								vertex = pointer2;
+								traversed = vertex;
+							} else {
+								int pointer3 = refs[refPointer + 3];
+								if (pointer3 > traversed) {
+									vertex = pointer3;
+									traversed = vertex;
+								} else {
+									int pointer4 = refs[refPointer + 4];
+									if (pointer4 > traversed) {
+										vertex = pointer4;
+										traversed = vertex;
+									} else {
+										vertex = refs[refPointer];
+									}
+								}
+							}
+						}
+					}
+				}
+
+			}
+			points[pointIndex + 2] += points[pointIndex + 4];
+			points[pointIndex + 3] += points[pointIndex + 5];
+			points[pointIndex + 0] += points[pointIndex + 2];
+			points[pointIndex + 1] += points[pointIndex + 3];
+		}
+
+	}
+
+	private static float computeForce(float mass1, float mass2, float dist) {
+		return (Gravity.G * mass1 * mass2) / (dist * dist);
+	}
+
+	private void computeAccelerationVector(float mass1, float mass2, float x1, float y1, float x2, float y2, int point,
+			float dist) {
+		if (dist == 0) {
+			return;
+		}
+		float force = computeForce(mass1, mass2, dist + Gravity.wall);
+		float ratio = force / (dist + Gravity.wall);
+
+		points[point + 4] += computeForceComponent(x1, x2, ratio) / mass1;
+		points[point + 5] += computeForceComponent(y1, y2, ratio) / mass1;
+	}
+
+	private static float computeForceComponent(float c1, float c2, float ratio) {
+
+		return (c2 - c1) * ratio;
+	}
+
+	public float[] getPoints() {
+		return points;
+	}
+
+	public void setPoints(float[] points) {
+		this.points = points;
+	}
+
+	public void setTree(float[] tree) {
+		this.tree = tree;
+	}
+
+	public void setRefs(int[] refs) {
+		this.refs = refs;
+	}
+
+	public void setRealSize(int realSize) {
+		this.realSize = realSize;
+	}
+
+}
--- src/ee/ut/jjanno/gpusimulation/GravitySystemGPUExecutor.java
+++ src/ee/ut/jjanno/gpusimulation/GravitySystemGPUExecutor.java
@@ -13,11 +13,13 @@ public class GravitySystemGPUExecutor {
 	static final GravitySystemKernel kernel = new GravitySystemKernel();
 	static final GravitySystemAdvancedKernel kernelAdvanced = new GravitySystemAdvancedKernel();
 	static final GravitySystemAdvancedUnstackKernel kernelAdvancedPlus = new GravitySystemAdvancedUnstackKernel();
+	static final GravitySystemAdvancedUnstackDuoKernel kernelAdvancedPlusDuo = new GravitySystemAdvancedUnstackDuoKernel();
 
 	static {
 		kernel.setExecutionMode(EXECUTION_MODE.GPU);
 		kernelAdvanced.setExecutionMode(EXECUTION_MODE.GPU);
 		kernelAdvancedPlus.setExecutionMode(EXECUTION_MODE.GPU);
+		kernelAdvancedPlusDuo.setExecutionMode(EXECUTION_MODE.GPU);
 	}
 
 	public static void execute(List<Body> bodies) {
@@ -31,7 +33,7 @@ public class GravitySystemGPUExecutor {
 	public static void executeAdvanced(List<Body> bodies) {
 		float[] extremes = View.getExtremes(bodies);
 		IndexSequence seq = new IndexSequence(0);
-		BodyTree tree = new BodyTree(bodies, seq, extremes[0], extremes[1], extremes[2], extremes[3], false);	
+		BodyTree tree = new BodyTree(bodies, seq, extremes[0], extremes[1], extremes[2], extremes[3], false);
 		kernelAdvanced.setPoints(parametrizeBodies(bodies));
 		kernelAdvanced.setTree(parametrizeTree(tree, seq.getLastIndex()));
 		int stackSize = 6400 + seq.getLastIndex() * seq.getLastIndex();
@@ -40,32 +42,47 @@ public class GravitySystemGPUExecutor {
 		kernelAdvanced.execute(bodies.size());
 		deParametrizeBodies(kernelAdvanced.getPoints(), bodies);
 	}
-	
+
 	public static void executeAdvancedPlus(List<Body> bodies) {
 		float[] extremes = View.getExtremes(bodies);
 		IndexSequence seq = new IndexSequence(1);
 		BodyTree tree = new BodyTree(bodies, seq, extremes[0], extremes[1], extremes[2], extremes[3], false);
 		kernelAdvancedPlus.setPoints(parametrizeBodies(bodies));
 		kernelAdvancedPlus.setTree(parametrizeTreeAdvanced(tree, seq.getLastIndex()));
-		kernelAdvancedPlus.setRealSize(bodies.size());	
-		kernelAdvancedPlus.execute(getClosestSize(bodies.size()));	
+		kernelAdvancedPlus.setRealSize(bodies.size());
+		kernelAdvancedPlus.execute(getClosestSize(bodies.size()));
 		deParametrizeBodies(kernelAdvancedPlus.getPoints(), bodies);
 	}
-	
+
 	public static void executeAdvancedPlusParallelTree(List<Body> bodies) {
 		float[] extremes = View.getExtremes(bodies);
 		IndexSequence seq = new IndexSequence(1);
 		BodyTreeParallel tree = new BodyTreeParallel(bodies, extremes[0], extremes[1], extremes[2], extremes[3]);
-		tree.index(seq);	
+		tree.index(seq);
 		kernelAdvancedPlus.setPoints(parametrizeBodies(bodies));
 		kernelAdvancedPlus.setTree(parametrizeTreeAdvanced(tree, seq.getLastIndex()));
-		kernelAdvancedPlus.setRealSize(bodies.size());	
-		kernelAdvancedPlus.execute(getClosestSize(bodies.size()));	
+		kernelAdvancedPlus.setRealSize(bodies.size());
+		kernelAdvancedPlus.execute(getClosestSize(bodies.size()));
 		deParametrizeBodies(kernelAdvancedPlus.getPoints(), bodies);
 	}
-	
+
+	public static void executeAdvancedPlusParallelTreeRefed(List<Body> bodies) {
+		float[] extremes = View.getExtremes(bodies);
+		IndexSequence seq = new IndexSequence(1);
+		BodyTreeParallel tree = new BodyTreeParallel(bodies, extremes[0], extremes[1], extremes[2], extremes[3]);
+		tree.index(seq);
+		kernelAdvancedPlusDuo.setPoints(parametrizeBodies(bodies));
+
+		kernelAdvancedPlusDuo.setTree(parametrizeTreeAdvancedUnrefed(tree, seq.getLastIndex()));
+		kernelAdvancedPlusDuo.setRefs(parametrizeTreeIndex(tree, seq.getLastIndex()));
+
+		kernelAdvancedPlusDuo.setRealSize(bodies.size());
+		kernelAdvancedPlusDuo.execute(getClosestSize(bodies.size()));
+		deParametrizeBodies(kernelAdvancedPlusDuo.getPoints(), bodies);
+	}
+
 	private static int getClosestSize(int size) {
-		return (int)(Math.pow(2, (int)(Math.log(size) / Math.log(2))+1));
+		return (int) (Math.pow(2, (int) (Math.log(size) / Math.log(2)) + 1));
 	}
 
 	private static float[] parametrizeBodies(List<Body> bodies) {
@@ -94,22 +111,88 @@ public class GravitySystemGPUExecutor {
 		return bodyList;
 	}
 
+	private static int[] parametrizeTreeIndex(BodyTreeParallel tree, int size) {
+		int[] treeArray = new int[size * 5];
+		fillTreeIndex(tree, treeArray);
+		return treeArray;
+	}
+
+	private static void fillTreeIndex(BodyTreeParallel tree, int[] treeArray) {
+		int pointer = (tree.index - 1) * 5;
+		treeArray[pointer] = tree.parent;
+
+		if (tree.lt != null) {
+			treeArray[pointer + 1] = tree.lt.index;
+			fillTreeIndex(tree.lt, treeArray);
+		} else {
+			treeArray[pointer + 1] = 0;
+		}
+
+		if (tree.lb != null) {
+			treeArray[pointer + 2] = tree.lb.index;
+			fillTreeIndex(tree.lb, treeArray);
+		} else {
+			treeArray[pointer + 2] = 0;
+		}
+
+		if (tree.rt != null) {
+			treeArray[pointer + 3] = tree.rt.index;
+			fillTreeIndex(tree.rt, treeArray);
+		} else {
+			treeArray[pointer + 3] = 0;
+		}
+
+		if (tree.rb != null) {
+			treeArray[pointer + 4] = tree.rb.index;
+			fillTreeIndex(tree.rb, treeArray);
+		} else {
+			treeArray[pointer + 4] = 0;
+		}
+	}
+
 	private static float[] parametrizeTree(BodyTree tree, int size) {
 		float[] treeArray = new float[size * 8];
 		fillTree(tree, treeArray);
 		return treeArray;
 	}
-	
+
 	private static float[] parametrizeTreeAdvanced(BodyTree tree, int size) {
 		float[] treeArray = new float[size * 9];
 		fillTreeAdvanced(tree, treeArray);
 		return treeArray;
 	}
-	
+
 	private static float[] parametrizeTreeAdvanced(BodyTreeParallel tree, int size) {
 		float[] treeArray = new float[size * 9];
 		fillTreeAdvanced(tree, treeArray);
 		return treeArray;
+	}
+
+	private static float[] parametrizeTreeAdvancedUnrefed(BodyTreeParallel tree, int size) {
+		float[] treeArray = new float[size * 4];
+		fillTreeAdvancedUnrefed(tree, treeArray);
+		return treeArray;
+	}
+
+	private static void fillTreeAdvancedUnrefed(BodyTreeParallel tree, float[] treeArray) {
+		int pointer = (tree.index - 1) * 4;
+		treeArray[pointer] = tree.x;
+		treeArray[pointer + 1] = tree.y;
+		treeArray[pointer + 2] = tree.mass;
+		treeArray[pointer + 3] = tree.width;
+
+		if (tree.lt != null)
+			fillTreeAdvancedUnrefed(tree.lt, treeArray);
+
+		if (tree.lb != null)
+			fillTreeAdvancedUnrefed(tree.lb, treeArray);
+
+		if (tree.rt != null)
+			fillTreeAdvancedUnrefed(tree.rt, treeArray);
+
+		if (tree.rb != null)
+			fillTreeAdvancedUnrefed(tree.rb, treeArray);
+
 	}
 
 	private static void fillTree(BodyTree tree, float[] treeArray) {
@@ -120,37 +203,37 @@ public class GravitySystemGPUExecutor {
 		treeArray[pointer + 7] = tree.width;
 
 		if (tree.lt != null) {
-			treeArray[pointer + 3] = tree.lt.index+1;
+			treeArray[pointer + 3] = tree.lt.index + 1;
 			fillTree(tree.lt, treeArray);
 		} else {
 			treeArray[pointer + 3] = 0;
 		}
-		
+
 		if (tree.lb != null) {
-			treeArray[pointer + 4] = tree.lb.index+1;
+			treeArray[pointer + 4] = tree.lb.index + 1;
 			fillTree(tree.lb, treeArray);
 		} else {
 			treeArray[pointer + 4] = 0;
 		}
-		
+
 		if (tree.rt != null) {
-			treeArray[pointer + 5] = tree.rt.index+1;
+			treeArray[pointer + 5] = tree.rt.index + 1;
 			fillTree(tree.rt, treeArray);
 		} else {
 			treeArray[pointer + 5] = 0;
 		}
-		
+
 		if (tree.rb != null) {
-			treeArray[pointer + 6] = tree.rb.index+1;
+			treeArray[pointer + 6] = tree.rb.index + 1;
 			fillTree(tree.rb, treeArray);
 		} else {
 			treeArray[pointer + 6] = 0;
 		}
 
 	}
-	
+
 	private static void fillTreeAdvanced(BodyTree tree, float[] treeArray) {
-		int pointer = (tree.index-1) * 9;
+		int pointer = (tree.index - 1) * 9;
 		treeArray[pointer] = tree.x;
 		treeArray[pointer + 1] = tree.y;
 		treeArray[pointer + 2] = tree.mass;
@@ -163,21 +246,21 @@ public class GravitySystemGPUExecutor {
 		} else {
 			treeArray[pointer + 3] = 0;
 		}
-		
+
 		if (tree.lb != null) {
 			treeArray[pointer + 4] = tree.lb.index;
 			fillTreeAdvanced(tree.lb, treeArray);
 		} else {
 			treeArray[pointer + 4] = 0;
 		}
-		
+
 		if (tree.rt != null) {
 			treeArray[pointer + 5] = tree.rt.index;
 			fillTreeAdvanced(tree.rt, treeArray);
 		} else {
 			treeArray[pointer + 5] = 0;
 		}
-		
+
 		if (tree.rb != null) {
 			treeArray[pointer + 6] = tree.rb.index;
 			fillTreeAdvanced(tree.rb, treeArray);
@@ -186,9 +269,9 @@ public class GravitySystemGPUExecutor {
 		}
 
 	}
-	
+
 	private static void fillTreeAdvanced(BodyTreeParallel tree, float[] treeArray) {
-		int pointer = (tree.index-1) * 9;
+		int pointer = (tree.index - 1) * 9;
 		treeArray[pointer] = tree.x;
 		treeArray[pointer + 1] = tree.y;
 		treeArray[pointer + 2] = tree.mass;
@@ -201,21 +284,21 @@ public class GravitySystemGPUExecutor {
 		} else {
 			treeArray[pointer + 3] = 0;
 		}
-		
+
 		if (tree.lb != null) {
 			treeArray[pointer + 4] = tree.lb.index;
 			fillTreeAdvanced(tree.lb, treeArray);
 		} else {
 			treeArray[pointer + 4] = 0;
 		}
-		
+
 		if (tree.rt != null) {
 			treeArray[pointer + 5] = tree.rt.index;
 			fillTreeAdvanced(tree.rt, treeArray);
 		} else {
 			treeArray[pointer + 5] = 0;
 		}
-		
+
 		if (tree.rb != null) {
 			treeArray[pointer + 6] = tree.rb.index;
 			fillTreeAdvanced(tree.rb, treeArray);
